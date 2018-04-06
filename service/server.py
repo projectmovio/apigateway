e48a5bd13afde8bb27eec1e50ab146d2ce4f4324
Michal Sadowski <michal.sadowski@axis.com>
2018-04-06 23:44:12 +0200

    Token inmem store

--- service/server.py
+++ service/server.py
@@ -1,19 +1,38 @@
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 
+from service.token_store import TokenStore
 from utils.log import Log
 
 app = Flask(__name__)
 log = Log().get_logger(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-@app.route("/", methods=["get"])
-def root():
-    return jsonify(requests.get("http://127.0.0.1:8080/movies").json())
+token_store = TokenStore()
 
 
 @app.route("/login", methods=["get"])
 def login():
-    return jsonify(requests.get("http://127.0.0.1:8081/login").json())
+    new_token = requests.get("http://127.0.0.1:8081/login").json()
+    token_store.add_token(new_token)
+    return jsonify(new_token)
+
+
+@app.route("/movies", methods=["get"])
+def movies():
+    log.debug("Request: %s", request)
+    log.debug("Headers: %s", request.headers)
+
+    token = request.headers.get("user_id")
+    log.debug("Token: %s", token)
+    if token == "":
+        log.debug("Empty token")
+        return Response("{'Error': 'Empty Token'}", status=401, mimetype='application/json')
+
+    valid = token_store.validate_token(token)
+    if not valid:
+        return Response("{'Error': 'Invalid Token'}", status=401, mimetype='application/json')
+    return jsonify(requests.get("http://127.0.0.1:8082/movies", headers=request.headers).json())
+
+
---
+++ service/token_store.py
@@ -0,0 +1,12 @@
+
+
+class TokenStore(object):
+
+    def __init__(self):
+        self.token_store = []
+
+    def add_token(self, token):
+        self.token_store.append(token)
+
+    def validate_token(self, token):
+        return token in self.token_store
